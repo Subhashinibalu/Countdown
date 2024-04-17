9d93e59388bd7e7840acdd5b793463d7444cbb26
Subhashinibalu <subhashiniulk@gmail.com>
2024-04-17 13:12:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const count = document.getElementById("count")
+
+
+setTimeout(()=>{
+    count.textContent="10";
+    setTimeout(()=>{
+        count.textContent="9";
+        setTimeout(()=>{
+            count.textContent="8";
+            setTimeout(()=>{
+                count.textContent="7";
+                setTimeout(()=>{
+                    count.textContent="6";
+                    setTimeout(()=>{
+                        count.textContent="5";
+                        setTimeout(()=>{
+                            count.textContent="4";
+                            setTimeout(()=>{
+                                count.textContent="3";
+                                setTimeout(()=>{
+                                    count.textContent="2";
+                                    setTimeout(()=>{
+                                        count.textContent="1";
+                                        setTimeout(()=>{
+                                            count.textContent="HAPPY INDEPENDENCE DAY ";
+                                            
+                                        
+                                        },1000)
+                                    
+                                    },1000)    
+                                
+                                },1000)
+                            
+                            },1000)
+                        
+                        },1000)
+                    
+                    },1000)
+                
+                },1000)
+            
+            },1000)
+        
+        },1000)
+    
+    },1000)
+
+},1000)
